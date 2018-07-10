f20e71e070874530362a8c3e2155cd6ec23ff2bd
K-25565 <34975675+K-25565@users.noreply.github.com>
2018-07-10 08:45:19 -0600

    line styles and arrow animations

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 
-export default class Arrow extends Component {
-   render() {
-      return (
-         <div className={`${this.props.className} arrow`}>
-            
-         </div>
-      );
-   }
+export default class Arrow extends Component {  
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: false
+        };
+    };
+
+    toggleArrowDirection = function () {
+        if (this.state.status) {
+            document.getElementById("arrow").classList.remove("arrow-closed");
+        } else {
+            document.getElementById("arrow").classList.add("arrow-closed");
+        };
+
+        this.setState({ status: !this.state.status });
+    }.bind(this);
+
+    render() {
+        return (
+            <a onClick={() => this.toggleArrowDirection()} className={`${this.props.className} arrow`} id="arrow"></a>
+        );
+    };
 };
